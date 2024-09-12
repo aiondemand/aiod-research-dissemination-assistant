92a5ae3bc64f83e72417a3c918fce603fc6b9362
Alexander Brecko <alexander.brecko@kinit.sk>
2024-09-12 18:12:37 +0200

    Add a new button for the summarization process. Multi-user support (#4)
    * Add button to summarize
    
    * Multi-user support
    
    * Stop/reset process options
    
    * Possibility to change the input file
    
    * Update errors and status codes
    
    ---------
    
    Co-authored-by: Brecko <alexander.brecko@gmail.com>
    Co-authored-by: Andrej Ridzik <andrej.ridzik@kinit.sk>

--- src/gradio/app/main.py
+++ src/gradio/app/main.py
@@ -4,7 +4,7 @@ import uuid
 
 import pymupdf
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from langchain_community.llms import Ollama
 
 import gradio as gr
@@ -20,38 +20,55 @@ logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
 
-async def process_pdf_and_summarize(file_content, session_id) -> str:
-    logging.info(f"Starting PDF processing for session {session_id}...")
-
-    # TODO: Change return of error messages by raising exception
+def validate_pdf_file(file_content):
     if not file_content:
-        logging.error("No file was uploaded.")
-        return "Error: No file was uploaded."
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No file was uploaded."
+        )
+    if not getattr(file_content, "name", "").lower().endswith(".pdf"):
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail="Uploaded file is not a PDF.",
+        )
 
-    elif not getattr(file_content, "name", "").lower().endswith(".pdf"):
-        logging.error("Uploaded file is not a PDF.")
-        return "Error: The uploaded file is not a PDF."
 
-    else:
+def extract_text_from_pdf(file_content):
+    try:
         pdf_document = pymupdf.open(file_content, filetype="pdf")
         text = ""
         for page_number in range(pdf_document.page_count):
             page = pdf_document.load_page(page_number)
             text += page.get_text("text")
         pdf_document.close()
+        return text
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error processing the PDF file: {str(e)}",
+        )
 
-        try:
-            loop = asyncio.get_running_loop()
-            summary_task = loop.run_in_executor(None, summarize_text, text)
-            summary_tasks[session_id] = summary_task
-            output_text = await summary_task
-            logging.info("Summarization completed.")
-            return output_text
-        except asyncio.CancelledError:
-            logging.warning(f"Summarization was cancelled for session {session_id}.")
-            return "Summarization processing was stopped by the user."
-        finally:
-            summary_tasks.pop(session_id, None)
+
+async def process_pdf_and_summarize(file_content, session_id) -> str:
+    global summary_tasks
+    logging.info(f"Starting PDF processing for session {session_id}...")
+
+    validate_pdf_file(file_content)  # validation input
+
+    text = extract_text_from_pdf(file_content)  # extract text from pdf
+
+    try:
+        loop = asyncio.get_running_loop()
+        task = loop.run_in_executor(None, summarize_text, text)
+        summary_tasks[session_id] = task
+        output_text = await task
+        logging.info("Summarization completed.")
+        return output_text
+    except Exception as e:
+        logging.error(f"Failed during processing: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to process PDF: {str(e)}",
+        )
 
 
 def prepare_post_text(
@@ -91,7 +108,7 @@ def prepare_post_text(
     if custom_requirements:
         post_text += f" Custom requirements: {custom_requirements}"
 
-    post_text += "skip introduction about audience, do not greet audience, finish with call to action"
+    post_text += "\n Skip introduction about audience, do not greet audience, finish with call to action"
 
     return post_text
 
@@ -112,133 +129,154 @@ async def generate_post_async(
     global llm_tasks
 
     if not summary:
-        return "Summarization is still in progress. Please wait and try again."
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Summarization not completed",
+        )
+    else:
+        post_text = prepare_post_text(
+            summary,
+            audience,
+            english_level,
+            length,
+            hashtag_preference,
+            perspective,
+            emoji_usage,
+            question_option,
+            paper_url,
+            custom_requirements,
+        )
 
-    post_text = prepare_post_text(
-        summary,
-        audience,
-        english_level,
-        length,
-        hashtag_preference,
-        perspective,
-        emoji_usage,
-        question_option,
-        paper_url,
-        custom_requirements,
-    )
+        llm = Ollama(
+            model=settings.generation_ollama_model,
+            base_url=settings.ollama_url,
+        )
 
-    llm = Ollama(
-        model=settings.generation_ollama_model,
-        base_url=settings.ollama_url,
-    )
-
-    try:
-        loop = asyncio.get_running_loop()
-        llm_task = loop.run_in_executor(None, llm.invoke, post_text)
-        llm_tasks[session_id] = llm_task
-        generated_post = await llm_task
-        return generated_post
-    except asyncio.CancelledError:
-        logging.warning(f"LLM processing was cancelled for session {session_id}.")
-        return "LLM processing was stopped by the user."
-    finally:
-        llm_tasks.pop(session_id, None)
+        try:
+            loop = asyncio.get_running_loop()
+            llm_task = loop.run_in_executor(None, llm.invoke, post_text)
+            llm_tasks[session_id] = llm_task
+            generated_post = await llm_task
+            return generated_post
+        except Exception as e:
+            logging.error(f"Failed post generation process: {str(e)}.")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed post generation process.",
+            )
 
 
 def stop_llm(session_id):
     global llm_tasks
     task = llm_tasks.get(session_id)
+
     if task is not None:
-        task.cancel()
-        return "LLM processing has been stopped."
+        llm_tasks.pop(session_id, None)
+        logging.info(
+            f"The post generation process for session {session_id} was cancelled."
+        )
     else:
-        logging.info(f"No LLM process was found running for session {session_id}.")
-        return "No active LLM process to stop."
+        logging.info(
+            f"No post generation process was found running for session {session_id}."
+        )
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No post generation running"
+        )
 
 
 async def reset_summarization(session_id):
     global summary_tasks
-    logging.info(summary_tasks)
     task = summary_tasks.get(session_id)
-    if task:
-        if not task.done():
-            task.cancel()
-            logging.info(f"Summarization task for session {session_id} was cancelled.")
+
+    if task is not None:
         summary_tasks.pop(session_id, None)
+        logging.info(f"Summarization task for session {session_id} was cancelled.")
     else:
         logging.info(
             f"No summarization process was found running for session {session_id}."
         )
-        return "No active summarization process to stop."
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No summarization running"
+        )
 
 
 with gr.Blocks(title="Research dissemination assistant") as demo:
     session_id = gr.State(lambda: uuid.uuid4().hex)
-    logging.info(session_id)
 
     gr.HTML(
         """
         <div style="display: flex; align-items: center;">
             <img src='file=Main_logo_RGB_colors.png' style='height: 100px; width: auto; alt='AI4EUROPE_logo'; margin-right: 20px;'/>
-            <h1 style="margin: 0; font-size: 24px;">Generate social media post</h1>
+            <h1 style="margin: 0; font-size: 24px;">QuickRePost</h1>
         </div>
         """
     )
+    first_part = gr.Group()
+    second_part = gr.Group()
 
-    pdf_input = gr.File(label="Upload your PDF Document")
-    summary_output = gr.Textbox(label="Summary of the PDF", lines=6)
-    reset_button = gr.Button("Reset")
+    with first_part:
+        gr.Markdown("## Document Summarization")
+        pdf_input = gr.File(label="Upload your PDF Document")
+        summary_output = gr.Textbox(label="Summary of the PDF", lines=6)
+        start_summarization_button = gr.Button("Start summarization")
+        reset_button = gr.Button("Reset")
 
-    audience_input = gr.Dropdown(
-        [
-            "High school student",
-            "University teacher",
-            "Researcher",
-            "Business executive",
-            "IT professional",
-            "Non-profit organization member",
-            "General public",
-        ],
-        label="Select your audience:",
-    )
-    english_level_input = gr.Dropdown(
-        ["Beginner", "Intermediate"], label="Select the English level:"
-    )
-    length_input = gr.Dropdown(
-        ["Long", "Short", "Very short"], label="Select the length of the post:"
-    )
-    hashtag_input = gr.Dropdown(
-        [
-            "No use hashtags",
-            "Use hashtags",
-            "Use 3 hashtags that are already existing, popular and relevant",
-        ],
-        label="Hashtag usage:",
-    )
-    perspective_input = gr.Dropdown(
-        ["First person singular", "First person plural", "Second person"],
-        label="Choose the narrative perspective:",
-    )
-    emoji_input = gr.Dropdown(
-        [
-            "Use emoticons",
-            "Use emoticons instead of bullet points",
-            "Do not use emoticons",
-        ],
-        label="Emoji usage:",
-    )
-    url_input = gr.Textbox(label="Paste the URL to your paper here:")
-    question_input = gr.Checkbox(label="Begin with Thought-Provoking Question.")
-    custom_requirements_input = gr.Textbox(
-        label="Enter your custom requirements here:", lines=4
-    )
+    with second_part:
+        gr.Markdown("## Social Media Post Panel")
 
-    submit_button = gr.Button("Generate post")
-    stop_button = gr.Button("Stop")
-    post_output = gr.Textbox(label="Generated LinkedIn Post", lines=8)
+        audience_input = gr.Dropdown(
+            [
+                "",
+                "High school student",
+                "University teacher",
+                "Researcher",
+                "Business executive",
+                "IT professional",
+                "Non-profit organization member",
+                "General public",
+            ],
+            label="Select your audience:",
+        )
+        english_level_input = gr.Dropdown(
+            ["", "Beginner", "Intermediate"], label="Select the English level:"
+        )
+        length_input = gr.Dropdown(
+            ["", "Long", "Short", "Very short"], label="Select the length of the post:"
+        )
+        hashtag_input = gr.Dropdown(
+            [
+                "",
+                "No use hashtags",
+                "Use hashtags",
+                "Use 3 hashtags that are already existing, popular and relevant",
+            ],
+            label="Hashtag usage:",
+        )
+        perspective_input = gr.Dropdown(
+            ["", "First person singular", "First person plural", "Second person"],
+            label="Choose the narrative perspective:",
+        )
+        emoji_input = gr.Dropdown(
+            [
+                "",
+                "Use emoticons",
+                "Use emoticons instead of bullet points",
+                "Do not use emoticons",
+            ],
+            label="Emoji usage:",
+        )
+        url_input = gr.Textbox(label="Paste the URL to your paper here:")
+        question_input = gr.Checkbox(label="Begin with Thought-Provoking Question.")
+        custom_requirements_input = gr.Textbox(
+            label="Enter your custom requirements here:", lines=4
+        )
 
-    click_event = pdf_input.change(
-        process_pdf_and_summarize,
+        submit_button = gr.Button("Generate post")
+        stop_button = gr.Button("Stop")
+        post_output = gr.Markdown(label="Generated LinkedIn Post")
+
+    click_event = start_summarization_button.click(
+        fn=process_pdf_and_summarize,
         inputs=[pdf_input, session_id],
         outputs=[summary_output],
         queue=True,
@@ -273,7 +311,15 @@ with gr.Blocks(title="Research dissemination assistant") as demo:
     )
 
     reset_button.click(
-        fn=lambda session_id: asyncio.run(reset_summarization(session_id)),
+        fn=reset_summarization,
+        inputs=[session_id],
+        outputs=[summary_output],
+        queue=False,
+        cancels=[click_event],
+    )
+
+    pdf_input.clear(
+        fn=reset_summarization,
         inputs=[session_id],
         outputs=[summary_output],
         queue=False,
@@ -285,6 +331,3 @@ app = gr.mount_gradio_app(app, demo, path="/", allowed_paths=["./"])
 # Run the interface
 if __name__ == "__main__":
     uvicorn.run(app, port=7860, log_level="debug")
-
-# TODO:
-# favicon_path="path-to-logo" as a parameter of demo.launch()
